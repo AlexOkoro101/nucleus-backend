5f2326f4763df2be98484953c9eabd0f966a1654
Alex <alexokoro342@gmail.com>
2021-12-10 20:39:02 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
+gulp.task("licenses", async function () {
+  // this is to add Creative Tim licenses in the production mode for the minified js
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/**/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Creative Tim licenses in the production mode for the minified html
+ 
+
+  // this is to add Creative Tim licenses in the production mode for the minified css
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
